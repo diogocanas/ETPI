c1f8812b4a6e5c44b701aa79541f0587fe5d4ef5
diogoalmeida1709 <diogoalmeida1709@gmail.com>
2020-05-13 17:58:58 +0200

    Connexion à la base de données

---
+++ src/php/inc.all.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Nom du projet  : ETPI
+ * Nom du fichier : inc.all.php
+ * Auteur         : Diogo Canas Almeida
+ * Date           : 13 mai 2020
+ * Description    : Fichier où se trouvent tout les include de fichiers
+ * Version        : 1.0
+ */
+
+ // Fichier de fonctions
+ include_once 'functions.php';
+
+ // Classes
+ include_once 'class/Database.php';
+
+ // Autres fichiers
+ include_once 'conparam.php';
+?>
